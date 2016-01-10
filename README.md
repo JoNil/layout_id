bce38240bc9f6524bec904035bc8189a643e0a30
Jonathan Nilsson <lord.jonil@gmail.com>
2016-01-10 16:39:10 +0100

    Updated license and dependencies

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Layout id is used to generate a unique id for a type that changes if it's memory
 
 ## License
 
-Layout id is distributed under the terms of the MIT license.
-
-See [LICENSE](LICENSE) for details.
+Layout id is Licensed under either of
+ * Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+at your option.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
+// Copyright (c) 2016 Jonathan Nilsson
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. All files in the project carrying such notice may not be copied,
+// modified, or distributed except according to those terms.
+
 #![feature(core_intrinsics)]
-#![feature(plugin)]
 
 #![cfg_attr(feature = "use_clippy", plugin(clippy))]
 
